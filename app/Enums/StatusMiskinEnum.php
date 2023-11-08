2e4d695eb6ba2b2a56114d2d42fb10819f5144fc
Hansen Makangiras <hansenmakangiras@gmail.com>
2023-11-09 03:16:33 +0800

    Add StatusMiskinEnum with labels, colors, and icons
    This commit introduces a new file 'StatusMiskinEnum.php' in the Enums directory. The file contains an enumerator class with four different status cases for poverty level along with its associated methods to get their labels, colors, and icons. This was added to centralize and standardize the handling of these status levels in the application code.

---
+++ app/Enums/StatusMiskinEnum.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Enums;
+
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+
+enum StatusMiskinEnum: string implements HasLabel, HasColor, HasIcon
+{
+
+    case MISKIN = 'MISKIN';
+    case RENTAN_MISKIN = 'RENTAN MISKIN';
+    case MAMPU = 'MAMPU';
+    case MISKIN_EKSTRIM = 'MISKIN EKSTRIM';
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::MISKIN => 'MISKIN',
+            self::RENTAN_MISKIN => 'RENTAN MISKIN',
+            self::MAMPU => 'MAMPU',
+            self::MISKIN_EKSTRIM => 'MISKIN EKSTRIM',
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::MISKIN => 'info',
+            self::RENTAN_MISKIN => 'warning',
+            self::MAMPU => 'danger',
+            self::MISKIN_EKSTRIM => 'success',
+        };
+    }
+
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::MISKIN => 'heroicon-o-arrow-path-rounded-square',
+            self::RENTAN_MISKIN => 'heroicon-o-user-minus',
+            self::MAMPU => 'heroicon-o-server-stack',
+            self::MISKIN_EKSTRIM => 'heroicon-o-check-circle',
+        };
+    }
+}
