82f9a78e5c9dce4ac0e18d3a635e23949ebc7387
Hanzo Alpha <hansen.makangiras@hotmail.com>
2024-01-14 01:32:17 +0800

    fix some migration field

--- app/Models/MutasiBpjs.php
+++ app/Models/MutasiBpjs.php
@@ -7,6 +7,7 @@ use App\Enums\JenisKelaminEnum;
 use App\Enums\StatusAktif;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MutasiBpjs extends Model
@@ -15,9 +16,15 @@ class MutasiBpjs extends Model
 
     protected $table = 'mutasi_bpjs';
 
+    protected $guarded = [];
+
     protected $casts = [
-        'jenis_kelamin' => JenisKelaminEnum::class,
         'alasan_mutasi' => AlasanEnum::class,
         'status_mutasi' => StatusAktif::class
     ];
+
+    public function peserta(): BelongsTo
+    {
+        return $this->belongsTo(PesertaBpjs::class);
+    }
 }
