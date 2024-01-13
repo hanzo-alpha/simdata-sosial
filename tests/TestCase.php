bf05c32bd06cd8d1628b91f063818eabc89dd62b
Hanzo Alpha <hansen.makangiras@hotmail.com>
2024-01-14 02:13:52 +0800

    remove not used widget

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,10 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use JMac\Testing\Traits\AdditionalAssertions;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, AdditionalAssertions;
+    use CreatesApplication, DatabaseTransactions;
 }
