55462b2c1b07bbbb20e25526e5ea6c458fe12f5a
Hanzo Alpha <hansen.makangiras@hotmail.com>
2024-05-14 02:23:47 +0800

    enable kelurahan chart dashboard

--- app/Filament/Widgets/BantuanSosialPerKelurahanChart.php
+++ app/Filament/Widgets/BantuanSosialPerKelurahanChart.php
@@ -23,7 +23,7 @@ class BantuanSosialPerKelurahanChart extends ApexChartWidget
 {
     use HasWidgetShield;
 
-    protected static bool $isDiscovered = false;
+    protected static bool $isDiscovered = true;
     protected static ?string $chartId = 'bantuanSosialPerKelurahanChart';
     protected static ?string $heading = 'Bantuan Sosial Per Kelurahan Chart';
     protected static ?int $sort = 3;
