315ed745a59a829dc602bd11952e57ca1c32ef92
hanzo-alpha <hanzo-alpha@users.noreply.github.com>
2024-05-27 07:21:50 +0000

    Apply pint changes

--- app/Policies/PenyaluranBantuanPpksPolicy.php
+++ app/Policies/PenyaluranBantuanPpksPolicy.php
@@ -2,8 +2,8 @@
 
 namespace App\Policies;
 
-use App\Models\User;
 use App\Models\PenyaluranBantuanPpks;
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class PenyaluranBantuanPpksPolicy
--- app/Policies/PesertaBpjsPolicy.php
+++ app/Policies/PesertaBpjsPolicy.php
@@ -2,8 +2,8 @@
 
 namespace App\Policies;
 
-use App\Models\User;
 use App\Models\PesertaBpjs;
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class PesertaBpjsPolicy
