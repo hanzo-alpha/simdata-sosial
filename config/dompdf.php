524ec2bdddba069155c5bb1b9b694a9c3c185aa6
Hanzo Alpha <hansen.makangiras@hotmail.com>
2024-09-29 02:30:53 +0800

    update package

--- config/dompdf.php
+++ config/dompdf.php
@@ -266,7 +266,7 @@ return [
          *
          * @var bool
          */
-        'enable_remote' => true,
+        'enable_remote' => false,
 
         /**
          * A ratio applied to the fonts height to be more like browsers' line height
